3c8a26a51541eb8cbad3b67ad5f12221b56482ab
huang0319 <289972168@qq.com>
2017-11-25 11:08:49 +0800

    1

--- src/main/java/com/didiapace/web/HelloController.java
+++ src/main/java/com/didiapace/web/HelloController.java
@@ -1,27 +1,19 @@
 package com.didiapace.web;
 
-import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.Configuration;
-import org.springframework.context.annotation.PropertySource;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.ResponseBody;
+
+/**
+ * @author Byron.Y.Y
+ * @date 2016年10月12日
+ */
 @Controller
-@SpringBootApplication
-@Configuration
-@PropertySource(value={"classpath:jdbc.properties","classpath:env.properties",
-		"classpath:httpclient.properties","classpath:redis.properities","classpath:rabbitmq.properties"})
 public class HelloController {
 
-	@RequestMapping("/hello")
-	@ResponseBody
-	public String index(){
-		
-		return "Hello SpringBoot222";
-	}
-	
-	public static void main(String[] args) {
-		SpringApplication.run(HelloController.class);
-	}
+    @RequestMapping("/hello")
+    public String hello(){
+        return "Hello, SpringBoot123!";
+        
+        
+    }
 }
