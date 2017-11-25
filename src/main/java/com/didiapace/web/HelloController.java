0ef49c8725428c59a8815856c6bce3938fb1fd01
huang0319 <289972168@qq.com>
2017-11-25 15:04:16 +0800

    dy

--- src/main/java/com/didiapace/web/HelloController.java
+++ src/main/java/com/didiapace/web/HelloController.java
@@ -1,5 +1,9 @@
 package com.didiapace.web;
 
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.PropertySource;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 
@@ -12,8 +16,6 @@ public class HelloController {
 
     @RequestMapping("/hello")
     public String hello(){
-        return "Hello, SpringBoot123!";
-        
-        
-    }
+        return "Hello, SpringBootccccc!";
+        }
 }
